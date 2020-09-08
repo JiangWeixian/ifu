261b87147ef1110ddcf1fc1887eb123addd247d7
jiangWeixian <jiangweixian1994@gmail.com>
2020-09-08 23:45:36 +0800

    ✨ (popcron): history mode

--- packages/popcron/index.ts
+++ packages/popcron/index.ts
@@ -2,18 +2,26 @@ import cronstrue from 'cronstrue/i18n'
 import alfy from 'alfy'
 
 const DEFAULT_LOCALE = 'en'
-const item = cronstrue.toString(alfy.input, { locale: process.env.locale || DEFAULT_LOCALE })
-
-alfy.output([
-  {
-    title: item,
-    arg: item,
+const KEY = 'history'
+const history = alfy.cache.get(KEY)
+if (!history) {
+  alfy.output(JSON.parse(history))
+} else {
+  const result = cronstrue.toString(alfy.input, { locale: process.env.locale || DEFAULT_LOCALE })
+  const item = {
+    title: result,
+    subtitle: alfy.input,
+    arg: result,
     icon: {
       path: ' ', // Hide icon
     },
     text: {
-      copy: item,
-      largetype: item,
+      copy: result,
+      largetype: result,
     },
-  },
-])
+  }
+
+  alfy.output([item])
+
+  alfy.cache.set(KEY, JSON.stringify([item].concat(history)))
+}
