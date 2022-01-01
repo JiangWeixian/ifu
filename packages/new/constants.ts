1183b8bbbc388a39fb8c2bf63218963dd42c67a4
jiangWeixian <jiangweixian1994@gmail.com>
2022-01-01 13:25:38 +0800

    :sparkles:  more shortcuts

--- packages/new/constants.ts
+++ packages/new/constants.ts
@@ -4,16 +4,56 @@ export const shortcuts = [
     name: 'Github Repo',
   },
   {
+    url: 'https://codespace.new',
+    name: 'Github Codespace',
+  },
+  {
     url: 'https://gist.new',
     name: 'Github Gist',
   },
   {
-    url: 'https://node.new',
-    name: 'Nodejs',
+    url: 'https://pen.new',
+    name: 'Codepen',
   },
   {
     url: 'https://deploy.new',
     name: 'Vercel',
+  },
+  {
+    url: 'https://react.new',
+    name: 'React CodeSandbox',
+  },
+  {
+    url: 'https://js.new',
+    name: 'JS CodeSandbox',
+  },
+  {
+    url: 'https://ts.new',
+    name: 'TS CodeSandbox',
+  },
+  {
+    url: 'https://ng.new',
+    name: 'Angular CodeSandbox',
+  },
+  {
+    url: 'https://vue.new',
+    name: 'Vue CodeSandbox',
+  },
+  {
+    url: 'https://python.new',
+    name: 'Python Playground',
+  },
+  {
+    url: 'https://repl.new',
+    name: 'Repl Playground',
+  },
+  {
+    url: 'https://linear.new',
+    name: 'Linear Dashboard',
+  },
+  {
+    url: 'https://node.new',
+    name: 'Nodejs',
   },
   {
     url: 'https://typescriptlang.org/play',
@@ -24,10 +64,6 @@ export const shortcuts = [
     name: 'Rust Playground',
   },
   {
-    url: 'https://python.new',
-    name: 'Python Playground',
-  },
-  {
     url: 'https://notion.new',
     name: 'Notion',
   },
@@ -36,11 +72,51 @@ export const shortcuts = [
     name: 'Google Doc',
   },
   {
-    url: 'https://figma.new',
-    name: 'Figma',
+    url: 'https://sheet.new',
+    name: 'Google Sheet',
+  },
+  {
+    url: 'https://slide.new',
+    name: 'Google Slide',
+  },
+  {
+    url: 'https://meet.new',
+    name: 'Google Meeting',
+  },
+  {
+    url: 'https://cal.new',
+    name: 'Google Calendar',
+  },
+  {
+    url: 'https://word.new',
+    name: 'Mircosoft Word',
+  },
+  {
+    url: 'https://execl.new',
+    name: 'Mircosoft Execl',
+  },
+  {
+    url: 'https://ppt.new',
+    name: 'Mircosoft PPT',
   },
   {
     url: 'https://story.new',
-    name: 'Medium',
+    name: 'Medium Post',
+  },
+  {
+    url: 'https://hackthon.new',
+    name: 'Hackthon',
+  },
+  {
+    url: 'https://figma.new',
+    name: 'Figma Design',
+  },
+  {
+    url: 'https://design.new',
+    name: 'Canva',
+  },
+  {
+    url: 'https://playlist.new',
+    name: 'Spotify Playlist',
   },
 ]
