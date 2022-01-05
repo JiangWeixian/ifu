ffe38bb6d56c4461e84b26cb144412be3d09bb6f
JiangWeixian <jiangweixian1994@gmail.com>
2022-01-05 13:52:50 +0800

    :bento:  brand icon

--- packages/new/constants.ts
+++ packages/new/constants.ts
@@ -2,50 +2,67 @@ export const shortcuts = [
   {
     url: 'https://repo.new',
     name: 'Github Repo',
+    icon: './icons/github.svg',
   },
   {
     url: 'https://codespace.new',
     name: 'Github Codespace',
+    icon: './icons/github.svg',
   },
   {
     url: 'https://gist.new',
     name: 'Github Gist',
+    icon: './icons/github.svg',
   },
   {
     url: 'https://pen.new',
     name: 'Codepen',
+    icon: './icons/codepen.svg',
   },
   {
     url: 'https://deploy.new',
     name: 'Vercel',
+    icon: './icons/vercel.svg',
   },
   {
     url: 'https://react.new',
     name: 'React CodeSandbox',
+    icon: './icons/react.svg',
   },
   {
     url: 'https://js.new',
     name: 'JS CodeSandbox',
+    icon: './icons/nodedotjs.svg',
   },
   {
     url: 'https://ts.new',
     name: 'TS CodeSandbox',
+    icon: './icons/typescript.svg',
   },
   {
     url: 'https://ng.new',
     name: 'Angular CodeSandbox',
+    icon: './icons/angular.svg',
   },
   {
     url: 'https://vue.new',
     name: 'Vue CodeSandbox',
+    icon: './icons/vuedotjs.svg',
+  },
+  {
+    url: 'https://nuxt.new',
+    name: 'Nuxt CodeSandbox',
+    icon: './icons/nuxtdotjs.svg',
   },
   {
     url: 'https://python.new',
     name: 'Python Playground',
+    icon: './icons/python.svg',
   },
   {
     url: 'https://repl.new',
     name: 'Repl Playground',
+    icon: './icons/replit.svg',
   },
   {
     url: 'https://linear.new',
@@ -54,54 +71,67 @@ export const shortcuts = [
   {
     url: 'https://node.new',
     name: 'Nodejs',
+    icon: './icons/nodedotjs.svg',
   },
   {
     url: 'https://typescriptlang.org/play',
     name: 'Typescript Playground',
+    icon: './icons/typescript.svg',
   },
   {
     url: 'https://play.rust-lang.org/',
     name: 'Rust Playground',
+    icon: './icons/rust.svg',
   },
   {
     url: 'https://notion.new',
     name: 'Notion',
+    icon: './icons/notion.svg',
   },
   {
     url: 'https://doc.new',
     name: 'Google Doc',
+    icon: './icons/google.svg',
   },
   {
     url: 'https://sheet.new',
     name: 'Google Sheet',
+    icon: './icons/googlesheets.svg',
   },
   {
     url: 'https://slide.new',
     name: 'Google Slide',
+    icon: './icons/google.svg',
   },
   {
     url: 'https://meet.new',
     name: 'Google Meeting',
+    icon: './icons/googlemeet.svg',
   },
   {
     url: 'https://cal.new',
     name: 'Google Calendar',
+    icon: './icons/googlecalendar.svg',
   },
   {
     url: 'https://word.new',
     name: 'Mircosoft Word',
+    icon: './icons/microsoftword.svg',
   },
   {
     url: 'https://execl.new',
     name: 'Mircosoft Execl',
+    icon: './icons/microsoftexcel.svg',
   },
   {
     url: 'https://ppt.new',
     name: 'Mircosoft PPT',
+    icon: './icons/microsoftpowerpoint.svg',
   },
   {
     url: 'https://story.new',
     name: 'Medium Post',
+    icon: './icons/medium.svg',
   },
   {
     url: 'https://hackathon.new',
@@ -110,13 +140,16 @@ export const shortcuts = [
   {
     url: 'https://figma.new',
     name: 'Figma Design',
+    icon: './icons/figma.svg',
   },
   {
     url: 'https://design.new',
     name: 'Canva',
+    icon: './icons/canva.svg',
   },
   {
     url: 'https://playlist.new',
     name: 'Spotify Playlist',
+    icon: './icons/spotify.svg',
   },
 ]
