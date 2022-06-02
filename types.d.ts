adaee3beaa5eab87161a3f781b7682edf8948702
jiangWeixian <jiangweixian1994@gmail.com>
2022-06-03 03:04:32 +0800

    :label:  declare module

--- types.d.ts
+++ types.d.ts
@@ -1,2 +1,7 @@
 declare module 'alfy'
 declare module 'alfy-test'
+declare module 'solarlunar' {
+  export const solar2lunar: (...args: any[]) => any
+}
+declare module 'suncalc'
+declare module 'alfy'
