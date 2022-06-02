08e7e86733b23256c9cd046d5837915b1e8b7ef3
JW <jiangweixian1994@gmail.com>
2022-06-03 02:29:17 +0800

    feat: logseq (#28)

---
+++ raycast/logseq/src/index.tsx
@@ -0,0 +1,20 @@
+import { Action, List, ActionPanel, Icon } from "@raycast/api";
+import { queryStatic7dRanges } from '@ifu/shared/logseq'
+
+export default function Command() {
+    return (
+      <List searchBarAccessory={
+        <List.Dropdown tooltip="Dropdown With Items">
+          <List.Dropdown.Item title="Query Weekly" value="query-weekly" />
+        </List.Dropdown>
+      }>
+        {queryStatic7dRanges().map(item => {
+          return <List.Item icon={Icon.Calendar} key={item} title={item} actions={
+            <ActionPanel>
+              <Action.CopyToClipboard content={item} />
+            </ActionPanel>
+          } />
+        })}
+      </List>
+  )
+}
